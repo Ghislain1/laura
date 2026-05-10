5b85c8e7e184f15ffea27556f38fdbd180c989a1
Ghislain <gzeleu@outlook.de>
2026-05-10 18:01:16 +0200

    ok

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ['react-app', '@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', '@typescript-eslint'],
+}
---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default tseslint.config(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            'react': react,
+            'react-hooks': reactHooks,
+        },
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+        },
+        settings: {
+            react: { version: 'detect' },
+        },
+    },
+    {
+        ignores: ['dist/**', 'node_modules/**'],
+    }
+);
